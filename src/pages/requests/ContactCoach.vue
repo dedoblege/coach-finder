f9a8e0b28aa3c2bc67b0d2152942e2192f0a43ff
dedoblege <dedoblege@gmail.com>
2020-10-12 10:59:31 +0200

    setting up store and coaches list

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,1 +1,3 @@
-
+<template>
+  CONTACT A COACH
+</template>
---
+++ src/store/modules/coaches/getters.js
@@ -0,0 +1,8 @@
+export default {
+  coaches(state) {
+    return state.coaches;
+  },
+  hascoaches(state) {
+    return state.coaches && state.coaches.length > 0;
+  }
+};
