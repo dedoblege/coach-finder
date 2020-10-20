ebe013ebed5c1879fbbdbae1131007f2b956ed10
dedoblege <dedoblege@gmail.com>
2020-10-20 09:02:42 +0200

    caching http response data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -30,7 +30,10 @@ export default {
       id: userId
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
     const response = await fetch(
       `https://vue-http-demo-bef34.firebaseio.com/coaches.json`
     );
@@ -56,5 +59,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   }
 };
